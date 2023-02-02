5826629a6dc2b8aac1f00a5fdc337cdec1805048
TheTeamInsane <115443754+TheTeamInsane@users.noreply.github.com>
2023-02-02 17:04:26 +0530

    Update active.py

--- InsaneMusic/plugins/tools/active.py
+++ InsaneMusic/plugins/tools/active.py
@@ -1,24 +1,17 @@
 # Kanged By © @always_hungry365
 # Owner Mayank
 # All rights reserved. © Alisha © Insane © Yukki
-import asyncio
-import math
-import os
-import shutil
-import socket
-import traceback
-import psutil
-import config
-from pyrogram import Client, filters
+
+
+
+from pyrogram import filters
 from pyrogram.types import Message
+
 from strings import get_command
 from InsaneMusic import app
 from InsaneMusic.misc import SUDOERS
 from InsaneMusic.utils.database.memorydatabase import (
-    get_active_chats as Active,
-    get_active_video_chats as activevideo,
-)
-from InsaneMusic.utils.command import commandpro
+    get_active_chats, get_active_video_chats)
 
 # Commands
 ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
@@ -27,8 +20,10 @@ ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 
 @app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
-    served_chats = await Active()
+    mystic = await message.reply_text(
+        "ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+    served_chats = await get_active_chats()
     text = ""
     j = 0
     for x in served_chats:
@@ -53,8 +48,10 @@ async def activevc(_, message: Message):
 
 @app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ʟɪsᴛ...")
-    served_chats = await activevideo()
+    mystic = await message.reply_text(
+        "ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+    served_chats = await get_active_video_chats()
     text = ""
     j = 0
     for x in served_chats:
@@ -75,45 +72,3 @@ async def activevi_(_, message: Message):
             f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :-**\n\n{text}",
             disable_web_page_preview=True,
         )
-
-
-LOGINGG = config.LOG_GROUP_ID
-
-
-# --------------------------Code------------------#
-
-
-@app.on_message(commandpro(["/ac"]) & SUDOERS)
-async def start(client: Client, message: Message):
-    ac_audio = str(len(active))
-    ac_video = str(len(activevideo))
-    await message.reply_text(
-        f"𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 📟\n•━━━━━━━━━━━━━━━━━━•\n🎙•Aᴜᴅɪᴏ  » {ac_audio} Gʀᴏᴜᴘs\n•───────•\n🖥• Vɪᴅᴇᴏ » {ac_video} Gʀᴏᴜᴘs\n•──────•",
-        quote=True,
-    )
-
-
-# --------------------------Clean_Commands------------------------#
-
-
-@app.on_message(commandpro(["/rm"]) & SUDOERS)
-async def cleaning(client: Client, message: Message):
-    A = "rm -rf downloads"
-    try:
-        os.system(A)
-    except:
-        await message.reply_text(
-            f"Failed To Delete Temp !!\nPlease Read\n{traceback.format_exc()}",
-            quote=True,
-        )
-    await message.reply_text(
-        f"Successfully Deleted Below Folders:\n -Downloads", quote=True
-    )
-
-
-CPU_LOAD = psutil.cpu_percent(interval=0.5)
-RAM_LOAD = psutil.virtual_memory().percent
-DISK_SPACE = psutil.disk_usage("/").percent
-
-
-# -----------------------------AUTO_CLEANER-&-SAFETY-------------------------------#
